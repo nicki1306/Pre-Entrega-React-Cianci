95c04497af62ca53416fabcc3dfe18d4f479690e
nicole <nickii.cn@hotmail.com>
2023-12-26 22:43:08 -0300

    navegacion

--- src/info/Pedirdata.js
+++ src/info/Pedirdata.js
@@ -8,3 +8,19 @@ export const Pedirdata = () => {
         })
     })
 }
+
+export const pedirItemPorId = (id) => {
+    return new Promise((resolve, reject) => {
+        
+        const item = data.find((el) => el.id === id);
+
+        if (item) {
+            resolve(item);
+        } else {
+            reject({
+                error: "No se encontró el producto"
+            })
+        }
+
+    })
+}
